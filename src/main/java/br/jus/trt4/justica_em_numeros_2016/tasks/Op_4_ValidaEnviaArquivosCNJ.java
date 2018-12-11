614ef213a89a42ca7f0984fbdba86d9c53c1df35
Felipe Luiz Christofolli Giotto <felipe.giotto@trt4.jus.br>
2018-12-11 15:07:59 -0200

    Certificados CNJ, refatoracoes

--- src/main/java/br/jus/trt4/justica_em_numeros_2016/tasks/Op_4_ValidaEnviaArquivosCNJ.java
+++ src/main/java/br/jus/trt4/justica_em_numeros_2016/tasks/Op_4_ValidaEnviaArquivosCNJ.java
@@ -13,7 +13,6 @@ import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
