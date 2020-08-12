41d73a2154946f41e6b36f28593a365c228acd75
Ivan <ilfncin@gmail.com>
2020-08-12 11:49:02 -0300

    Ajuste nos sqls

--- src/main/resources/sql/op_1_baixa_lista_processos/legado/carga_completa_migrados.sql
+++ src/main/resources/sql/op_1_baixa_lista_processos/legado/carga_completa_migrados.sql
@@ -2,5 +2,7 @@ SELECT
     nr_processo
 FROM
     stage_legado_1grau.processo
-WHERE migrado = true
-and proc_localizado_siaj = 'S'
+WHERE 
+	1=1
+	and proc_localizado_siaj 	= 'S'
+	and proc_hibrido			= 'S'
--- src/main/resources/sql/op_1_baixa_lista_processos/legado/carga_completa_nao_migrados.sql
+++ src/main/resources/sql/op_1_baixa_lista_processos/legado/carga_completa_nao_migrados.sql
@@ -1,6 +1,9 @@
-SELECT
-    nr_processo
-FROM
-    stage_legado_1grau.processo
-WHERE migrado = false
-and proc_localizado_siaj = 'S'
+select 
+	nr_processo
+from 
+	stage_legado_1grau.processo 
+where 
+	1=1
+	and proc_localizado_siaj 	= 'S'
+	and proc_escopo_legado 		= 'S'
+	and proc_hibrido			= 'N'
