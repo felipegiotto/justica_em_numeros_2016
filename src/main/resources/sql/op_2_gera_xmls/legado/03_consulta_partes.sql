218f5a903f29a5fc26b7ea9cd10f145e598186f4
Ivan <ilfncin@gmail.com>
2020-08-13 11:10:59 -0300

    Ajuste em consulta do legado

--- src/main/resources/sql/op_2_gera_xmls/legado/03_consulta_partes.sql
+++ src/main/resources/sql/op_2_gera_xmls/legado/03_consulta_partes.sql
@@ -2,11 +2,11 @@ SELECT
 	p.nr_processo,
     pp.in_participacao,	
 	pe.id_pessoa, 
-    pe.ds_nome,
-    pe.ds_nome          AS ds_nome_consulta,
+    case when pe.ds_nome is null then '' else pe.ds_nome end as ds_nome,
+    case when pe.ds_nome is null then '' else pe.ds_nome end as ds_nome_consulta,
     pe.in_tipo_pessoa   AS in_tipo_pessoa,
     'D' AS in_sexo, -- não possuímos tal informação
-    pp.id_processo_parte AS id_processo_parte,
+    (select max(pp2.id_processo_parte) from stage_legado_1grau.processo_parte pp2 where pp.cd_processo = pp2.cd_processo and pp.cd_pessoa = pp2.cd_pessoa)  AS id_processo_parte,
     (select pp2.id_processo_parte from stage_legado_1grau.processo_parte pp2 where pp2.cd_processo_parte = pp.cd_proc_parte_representante ) AS id_parte_representante, 
     pp.ds_tipo_parte_representante AS ds_tipo_parte_representante,
     CAST(null AS DATE) AS dt_nascimento, -- não possuímos tal informação
@@ -22,5 +22,3 @@ where
 	and p.cd_processo = pp.cd_processo
 	and pp.cd_pessoa = pe.cd_pessoa
 	and p.nr_processo = ANY(:numeros_processos)
-	
-	
