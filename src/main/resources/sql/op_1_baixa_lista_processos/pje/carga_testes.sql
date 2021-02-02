48a5dfbdbdbf047a743c7ee8cd7ff832dc5357ce
Wiler Coelho <wiler.coelho@trt7.jus.br>
2021-02-02 11:07:16 -0300

    Pequena correção no script de carga de testes da operação 1.

--- src/main/resources/sql/op_1_baixa_lista_processos/pje/carga_testes.sql
+++ src/main/resources/sql/op_1_baixa_lista_processos/pje/carga_testes.sql
@@ -1,6 +1,6 @@
 SELECT nr_processo
 FROM tb_processo p
-INNER JOIN tb_processo_trf ptrf ON (p.id_processo = ptrf.id_processo_trf)
+INNER JOIN tb_processo_trf ptrf ON (p.id_processo = ptrf.id_processo_trf AND ptrf.dt_autuacao IS NOT NULL)
 WHERE nr_ano = 2016
 ORDER BY random()
 LIMIT 100
